c719822d7c64d7f4eb739f9c5b4ba4ada903ca93
gabriel-wer <user@host.local>
2023-10-23 22:30:20 -0300

    change: moved from gORM to the default database module

--- backend/main.go
+++ backend/main.go
@@ -6,14 +6,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+
+	"database/sql"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "github.com/lib/pq"
 )
 
-var db *gorm.DB
+var db *sql.DB;
 
 type URL struct { 
     ShortURL string `json:"shorturl" gorm:"primaryKey;index"`
@@ -32,9 +34,9 @@ func shorten(w http.ResponseWriter, r *http.Request) {
     hash := sha1.Sum([]byte(url.LongURL))
     url.ShortURL = fmt.Sprintf("%x", hash)[:6]
 
-    result := db.Create(&url)
-    if result.Error != nil {
-        http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+    _, err = db.Exec("INSERT INTO url (shorturl, longurl) VALUES ($1, $2)", url.ShortURL, url.LongURL)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
@@ -52,24 +54,36 @@ func redirect(w http.ResponseWriter, r *http.Request) {
     var url URL
     url.ShortURL = chi.URLParam(r, "url")
 
-    db.Where("short_url = ?", url.ShortURL).Find(&url)
+    db.QueryRow("SELECT longurl FROM url WHERE shorturl = $1", url.ShortURL).Scan(&url.LongURL)
+    if !strings.Contains(url.LongURL, "https") || !strings.Contains(url.LongURL, "http") {
+        url.LongURL = "https://" + url.LongURL
+    }
+    fmt.Println(url.LongURL)
     http.Redirect(w, r, url.LongURL, http.StatusSeeOther)
 }
 
-func main() {
-    dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s", 
+func setup() *sql.DB{
+    dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=picori sslmode=disable", 
                         os.Getenv("DB_HOST"), 
                         os.Getenv("DB_USER"), 
                         os.Getenv("DB_PASSWORD"), 
                         os.Getenv("DB_PORT"))
-    var err error
-    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
+    db, err := sql.Open("postgres", dsn)
     if err != nil {
         panic(err)
     }
 
-    db.AutoMigrate(&URL{})
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS url (shorturl TEXT, longurl TEXT, PRIMARY KEY(shorturl))")
+    if err != nil {
+        panic(err)
+    }
+
+    return db
+}
+
+func main() {
+    db = setup()
+    defer db.Close()
 
     r := chi.NewRouter()
     r.Use(middleware.Logger)
